18abec21e55e8fd70a275abe086d023d13b9ee4b
Konstantin Rybakov <konstantin.rybakov@nmi.com>
2025-04-02 20:52:01 +0300

    added CORS

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from src.api import contacts, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["<http://localhost:3000>"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(contacts.router, prefix="/api")
 app.include_router(auth.router, prefix="/api")
